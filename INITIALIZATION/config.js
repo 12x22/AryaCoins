41b9e26bcd7fc72ef5a462e84a815cc318cfae84
12x26 <84988586+12x26@users.noreply.github.com>
2021-05-28 19:50:35 +0200

    Add files via upload

---
+++ INITIALIZATION/config.js
@@ -0,0 +1,47 @@
+/////* [CONFIGURATION] */////
+
+///* [TOKEN] *///
+module.exports.TOKEN = "Nzc2MDcwNjQ5NjI1NzA2NDk3.X6viHQ.EqyYiaAMwwzz_uFd9ESXYZJFSC8";
+
+///* [WEBHOOK] *///
+module.exports.WEBHOOK = "https://discord.com/api/webhooks/847204048510779402/Fq68aGidAYdEHYoNR_YyfnIMuuYofqBnQTvYRFZ787yZi0ga7FXA1Yy4YOVfoscy2hx0";
+
+///* [OWNERS] *///
+module.exports.OWNERS = ["787326329510297620"];
+
+///* [CATÉGORIE PERSO] *///
+module.exports.PERSOCAT = "847165185370095696";
+module.exports.PERSO_ROLE_POS = "51";
+
+///* [ROULETTE] *///
+module.exports.ROULETTE = "847165110580805663";
+
+///* [COINFLIP] *///
+module.exports.COINFLIP = "847165068051218483";
+
+
+///* [SERVEUR] *///
+module.exports.MAINGUILD = "847067754715676692";
+
+///* [CATÉGORIE TEAM] *///
+module.exports.TEAMCAT = "847535337315500053";
+module.exports.TEAM_ROLE_POS = "50";
+
+///* [DBLTOKEN] *///
+module.exports.DBLTOKEN = "";
+
+
+///* [DATABASE] *///
+module.exports.DATABASE_0 = {
+    host: 'localhost',
+    user: 'root',
+    database: 'zrxyns',
+    password: ""
+};
+
+module.exports.DATABASE_1 = {
+    host: 'localhost',
+    user: 'root',
+    database: 'zrxyns',
+    password: ""
+};
